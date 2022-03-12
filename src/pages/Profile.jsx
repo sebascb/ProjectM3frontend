2e860b80c788df418fc545084c346648cc597401
joaquinlmartin <joaquinlmartin@gmail.com>
2022-03-12 11:59:14 +0100

    profile extended

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,7 +6,9 @@ import apiService from "../services/api.service";
 
 function Profile () {
   const [userProfile, setProfileUser] = useState([]);
+  const [showFavs, setShowFavs] = useState([]);
   const { userId } = useParams();
+  const { cardId } = useParams();
   const { user } = useContext(AuthContext);
 
   useEffect(() => {
@@ -20,22 +22,37 @@ function Profile () {
       });
   }, [userId]);
 
+const getCardFavorite = async () => {
+    try {
+      const response = await apiService.getFavorite(cardId);
+      setShowFavs(response.data)
+    }catch(error){
+      console.log(error)
+    }
+  };
+  
+  useEffect(() => {
+    getCardFavorite();
+  }, [cardId]);
+
    return (
-              <div>
-               {/* <h2>Profile</h2> 
-                <p>{favoriteCard.name}</p>
-                  <div>
-                    <img src={favoriteCard.image} style={{ width: '200px'}} alt={favoriteCard.name} />
-                  </div>
-                  <div>
-                    <p>{favoriteCard.element}</p>
-                    <p>{favoriteCard.decription}</p>
-                    <p>{favoriteCard.attack}</p>
-                    <p>{favoriteCard.hp}</p>
-                    <p>{favoriteCard.ability}</p> */}
+           <div>
+              <h2>Profile</h2> 
+               <p>{showFavs.name}</p>
+                 <div>
+                   <img src={showFavs.image} style={{ width: '200px'}} alt={showFavs.name} />
+                 </div>
+                 <div>
+                   <p>{showFavs.element}</p>
+                   <p>{showFavs.decription}</p>
+                   <p>{showFavs.attack}</p>
+                   <p>{showFavs.hp}</p>
+                   <p>{showFavs.ability}</p>
+                   </div>
                <p>{userProfile.name}</p>
                <p>{user.name}</p>
                <p>{user.email}</p>
+                <p>{user.card}</p>
               </div>
       );
   }
