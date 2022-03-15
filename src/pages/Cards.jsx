cf5eeeecc939ca7ec585f7fe41191bb8a7ef1ff2
joaquinlmartin <joaquinlmartin@gmail.com>
2022-03-15 19:01:12 +0100

    searchbar extended

--- src/pages/Cards.jsx
+++ src/pages/Cards.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import apiService from "../services/api.service";
-import SearchBar from '../components/SearchBar';
 
 function Cards () {
   const [cards, setCards] = useState([]);
+  const [input, setInput] = useState('');
  // const apiURL = "https://cranky-lewin-d3d1ec.netlify.app/cards";
 
   useEffect(() => {
@@ -16,13 +16,31 @@ function Cards () {
     .catch((error) => console.log(error))
   }, []);
 
+  const handleBar = (e) => {
+    setInput(e.target.value);
+    filterCard(e.target.value);
+  };
+
+  const filterCard = (input) => {
+    setCards(cards.filter((card) => card.name.includes(input)));
+    if (input === '') {
+      setCards(cards);
+    } else {
+      setCards(
+        cards.filter((card) =>
+          card.name.toLowerCase().includes(input.toLowerCase())
+        )
+      )
+    }
+  };
+
   return (
         <div>
           <div className="title-card">
             <h2 className="letter-card">Pokemon Cards</h2>
           </div>
           <div className="searchBar"> 
-            <SearchBar />
+          <input type="text" name="name" value={input} onChange={handleBar} placeholder="search" />
           </div>
           <div className="button-main-top">
             <Link to="/cards/create">
@@ -41,6 +59,7 @@ function Cards () {
               </div>
             ))}
           </div>
+
         </div>
       );
     }
