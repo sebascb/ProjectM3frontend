9d7b4de533983e884fbc0fcc00df7710c1e8001b
joaquinlmartin <joaquinlmartin@gmail.com>
2022-03-16 14:40:47 +0100

    navbar styles added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,21 +12,20 @@ function Navbar() {
      <nav className="flex items-center justify-between flex-wrap bg-gray-700 p-6">
        
        <div className="flex items-center flex-shrink-0 text-white mr-6">
-    <img className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" src={Balls}></img>
-    <span className="font-semibold text-xl tracking-tight">Pokemon App</span>
+    <img className="fill-current h-8 w-8 mr-2 App-logo"  width="54" height="54" viewBox="0 0 54 54" src={Balls}></img>
+    <span className="font-semibold text-xl tracking-tight"><Link to="/"><button>Pokemon App</button>
+      </Link></span>
       </div>
       
-      <div className="block lg:hidden">
-         <Link to="/">
+      {/* <div className="block lg:show">
+         <Link to="/"> className="fill-current h-8 w-8 mr-2"
      <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
       <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menú</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
     </button>
      </Link>
-      </div>
+      </div> */}
 
-      <Link to="/" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
-        <button>Home</button>
-      </Link>
+      
 
 <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
    <div className="text-sm lg:flex-grow">
