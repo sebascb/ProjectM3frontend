8f93ccbe72aa3918227106fd13183d4fe55671e7
Sebas Corral <sebascorralbrito@gmail.com>
2022-03-18 14:23:07 +0100

    navbar styles extended

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,36 +9,36 @@ function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-     <nav className="flex items-center justify-between flex-wrap bg-gray-700 p-6">      
-       <div className="flex items-center flex-shrink-0 text-white mr-6">
+     <nav className="flex items-center flex-start flex-wrap bg-gray-700 p-2">  
+       <div className="flex items-center flex-shrink-0 text-white mr-3">
        <span className="font-semibold text-xl tracking-tight"><Link to="/"><img className="fill-current h-8 w-8 mr-2 App-logo"  width="54" height="54" viewBox="0 0 54 54" src={Balls}></img>
        </Link></span>
        </div>
 
-<div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-   <div className="text-sm lg:flex-grow">
+<div className="inline-flex ">
+   <div className="text-sm sm:flex-grow">
       {isLoggedIn && (
         <>
-          <Link to="/cards" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          <Link to="/cards" className="inline-flex items-center justify-center p-2 text-teal-200 hover:text-white mr-2">
             <button>Cards</button>
           </Link>
-          <Link to="/cards/create" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          <Link to="/cards/create" className="inline-flex items-center justify-center p-2 text-teal-200 hover:text-white mr-2">
             <button>Create</button>
           </Link>
-          <Link to="/profile" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          <Link to="/profile" className="inline-flex items-center justify-center p-2 text-teal-200 hover:text-white mr-2">
             <button>Profile</button>
           </Link>
-          <span className="block mt-4 lg:inline-block lg:mt-0 text-orange-600 hover:text-white mr-4">{user && user.name}</span>
+          <span className="inline-flex items-center justify-center p-2 text-orange-600 hover:text-white mr-4">{user && user.name}</span>
         </>
       )}
 </div>
       {!isLoggedIn && (
         <>
-          <Link to="/signup" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          <Link to="/signup" className="inline-flex items-center justify-center p-2 text-teal-200 hover:text-white mr-4">
             {' '}
             <button>Sign Up</button>{' '}
           </Link>
-          <Link to="/login" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+          <Link to="/login" className="inline-flex items-center justify-center p-2 text-teal-200 hover:text-white mr-4">
             {' '}
             <button>Login</button>{' '}
           </Link>
@@ -47,7 +47,7 @@ function Navbar() {
 
     </div>  
     <div>
-       <button onClick={logOutUser} className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"><svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth="2">
+       <button onClick={logOutUser} className="inline-flex items-center justify-center p-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mr-2 "><svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth="2">
   <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
 </svg></button>
     </div>
