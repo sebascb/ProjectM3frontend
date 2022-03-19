b5af9df9a8b7619a5885aa4bf6363e65ce6e3566
Sebas Corral <sebascorralbrito@gmail.com>
2022-03-19 11:36:33 +0100

    navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,8 +46,8 @@ function Navbar() {
       )}
 
     </div>  
-    <div>
-       <button onClick={logOutUser} className="inline-flex items-center justify-center p-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white ml-14 "><svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth="2">
+    <div className='mr-0'>
+       <button onClick={logOutUser} className="inline-flex items-center justify-center p-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white ml-12 "><svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth="2">
   <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
 </svg></button>
     </div>
