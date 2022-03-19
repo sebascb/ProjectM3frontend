7a5a22a07acd3b21b775cee2886f1631f279e3f6
Sebas Corral <sebascorralbrito@gmail.com>
2022-03-19 11:28:43 +0100

    styles updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-     <nav className="flex items-center flex-start flex-wrap bg-gray-700 p-2">  
+     <nav className="flex items-center flex-start flex-wrap bg-gray-700 p-3">  
        <div className="flex items-center flex-shrink-0 text-white mr-0">
        <span className="font-semibold text-xl tracking-tight"><Link to="/"><img className="fill-current h-8 w-8 mr-2 App-logo"  width="54" height="54" viewBox="0 0 54 54" src={Balls}></img>
        </Link></span>
@@ -47,7 +47,7 @@ function Navbar() {
 
     </div>  
     <div>
-       <button onClick={logOutUser} className="inline-flex items-center justify-center p-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mr-2 "><svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth="2">
+       <button onClick={logOutUser} className="inline-flex items-center justify-center p-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white ml-14 "><svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 20 20" stroke="currentColor" strokeWidth="2">
   <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
 </svg></button>
     </div>
