26b4c5d180a5f0364847c880b942209f7d26a629
joaquinlmartin <joaquinlmartin@gmail.com>
2022-03-23 20:42:37 +0100

    navigate on logout added

--- src/context/auth.context.jsx
+++ src/context/auth.context.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import apiService from '../services/api.service';
+import { useNavigate } from 'react-router-dom';
 
 const AuthContext = React.createContext();
 
@@ -7,6 +8,7 @@ function AuthProviderWrapper(props) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   const storeToken = token => {
     localStorage.setItem('authToken', token);
@@ -52,6 +54,7 @@ function AuthProviderWrapper(props) {
   const logOutUser = () => {
     removeToken();
     authenticateUser();
+    navigate('/');
   };
 
   useEffect(() => {
